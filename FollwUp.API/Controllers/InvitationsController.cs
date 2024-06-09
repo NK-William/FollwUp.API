9bc4bf9a6e0a3480d6441f3639c725b80e904a70
Nkuna William <64930245+NK-William@users.noreply.github.com>
2024-06-09 21:29:57 +0200

    Added invitation controller and DTO

---
+++ FollwUp.API/Controllers/InvitationsController.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using FollwUp.API.Model.Domain;
+using FollwUp.API.Model.DTO;
+using FollwUp.API.Repositories.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FollwUp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InvitationsController : ControllerBase
+    {
+        private readonly IMapper mapper;
+        private readonly IInvitationRepository invitationRepository;
+
+        public InvitationsController(IMapper mapper, IInvitationRepository invitationRepository)
+        {
+            this.mapper = mapper;
+            this.invitationRepository = invitationRepository;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] AddInvitationRequestDto addInvitationRequestDto)
+        {
+            var invitationDomainModel = mapper.Map<Invitation>(addInvitationRequestDto);
+
+            await invitationRepository.CreateAsync(invitationDomainModel);
+
+            var invitationDto = mapper.Map<InvitationDto>(invitationDomainModel);
+
+            return Ok(invitationDto);
+        }
+    }
+}
---
+++ FollwUp.API/Model/DTO/InvitationDto.cs
@@ -0,0 +1,9 @@
+﻿namespace FollwUp.API.Model.DTO
+{
+    public class InvitationDto
+    {
+        public Guid Id { get; set; }
+        public required string PhoneNumber { get; set; }
+        public Guid TaskId { get; set; }
+    }
+}
