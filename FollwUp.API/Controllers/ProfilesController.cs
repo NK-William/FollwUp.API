2af694f39c02ebe985041d3572b21a065093cdce
Nkuna William <64930245+NK-William@users.noreply.github.com>
2024-08-13 22:25:58 +0200

    Profile info by email address

--- FollwUp.API/Controllers/ProfilesController.cs
+++ FollwUp.API/Controllers/ProfilesController.cs
@@ -23,9 +23,29 @@ namespace FollwUp.API.Controllers
         [HttpPost]
         public async Task<IActionResult> Create([FromBody] AddProfileRequestDto addProfileRequestDto)
         {
-            var profileDomainModel = mapper.Map<Model.Domain.Profile>(addProfileRequestDto);
+            try
+            {
+                var profileDomainModel = mapper.Map<Model.Domain.Profile>(addProfileRequestDto);
 
-            await profileRepository.CreateAsync(profileDomainModel);
+                await profileRepository.CreateAsync(profileDomainModel);
+
+                var profileDto = mapper.Map<ProfileDto>(profileDomainModel);
+
+                return Ok(profileDto);
+            }catch(Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpGet]
+        [Route("ByEmail/{email}")]
+        public async Task<IActionResult> GetByEmail([FromRoute] string email)
+        {
+            var profileDomainModel = await profileRepository.GetByEmailAsync(email);
+
+            if (profileDomainModel == null)
+                return NotFound("Profile not found");
 
             var profileDto = mapper.Map<ProfileDto>(profileDomainModel);
 
