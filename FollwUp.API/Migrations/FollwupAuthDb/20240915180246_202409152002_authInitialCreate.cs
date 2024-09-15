617c2b05f2c97d67bc02b90d35dc995cac613e72
Nkuna William <64930245+NK-William@users.noreply.github.com>
2024-09-15 21:15:45 +0200

    Migration

---
+++ FollwUp.API/Migrations/FollwupAuthDb/20240915180246_202409152002_authInitialCreate.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FollwUp.API.Migrations.FollwupAuthDb
+{
+    /// <inheritdoc />
+    public partial class _202409152002_authInitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
