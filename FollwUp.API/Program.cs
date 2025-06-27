f467966b723452e21d2e1d0d1b2e57fff9d05041
Nkuna William <64930245+NK-William@users.noreply.github.com>
2025-06-27 10:53:48 +0200

    chore: disable HTTPS redirection in production environment

--- FollwUp.API/Program.cs
+++ FollwUp.API/Program.cs
@@ -136,13 +136,14 @@ app.Urls.Add($"http://*:{port}");
 app.UseCors("AllowFrontend");
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment() ||  app.Environment.IsProduction())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
 
-app.UseHttpsRedirection();
+app.UseSwagger();
+app.UseSwaggerUI();
+
+if(app.Environment.IsDevelopment())
+{
+    app.UseHttpsRedirection();
+}
 
 app.UseAuthentication();
 
